acb8113e2b7920b89b38ac46d12ffb71d5f8a2f4
Felicitas <felicitas.aguerralde@gmail.com>
2023-05-16 16:50:06 -0300

    "Agrego consultas"

--- AppData/Roaming/JetBrains/DataGrip2023.1/consoles/db/1f30036c-8318-4a98-acf8-8dc549a13a32/console_11.sql
+++ AppData/Roaming/JetBrains/DataGrip2023.1/consoles/db/1f30036c-8318-4a98-acf8-8dc549a13a32/console_11.sql
@@ -80,3 +80,42 @@ ALTER TABLE EJ_TRABAJA ADD CONSTRAINT FK_TRABAJA_PROYECTO
 ;
 
 -- End of file.
+
+insert into ej_ingeniero (id_ingeniero, nombre, apellido, contacto, especialidad, remuneracion)
+values (4, 'Sebastian', 'Luguercio', 2494121536, 'DESARROLLO', 900000);
+insert into ej_sector (id_sector, descripcion, ubicacion, cant_empleados) values (3,'DESARROLLO', 'Mar del Plata', 500);
+insert into ej_proyecto (id_sector, nro_proyecto, nombre, presupuesto, fecha_ini, fecha_fin, director)
+VALUES (3,6,'REDESI', 50000, '2022-02-25', NULL, 2);
+select * from ej_proyecto;
+select * from ej_ingeniero;
+select * from ej_trabaja;
+select * from ej_sector;
+insert into ej_trabaja (horas_sem, id_ingeniero, id_sector, nro_proyecto) VALUES (45,3,1,5);
+update ej_ingeniero set especialidad='GESTIÓN IT' where id_ingeniero=3;
+update ej_ingeniero set remuneracion=150000 where id_ingeniero=4;
+update ej_proyecto set presupuesto=80000 where nro_proyecto=6;
+
+--Las especialidades de los ingenieros pueden ser "INTELIGENCIA EMPRESARIAL" , "TECNOLOGÍAS MOVILES" , "GESTIÓN IT" o "DESARROLLO"
+ALTER TABLE ej_ingeniero ADD CONSTRAINT ck_ej_ingeniero_especialidad CHECK (especialidad IN ('INTELIGENCIA EMPRESARIAL', 'TECNOLOGÍAS MOVILES', 'GESTIÓN IT', 'DESARROLLO'));
+
+--La remuneración de un ingeniero debe ser mayor o igual a 25000$ y menor o igual a 250.000$
+ALTER TABLE ej_ingeniero ADD CONSTRAINT ck_ej_ingeniero_remuneracion CHECK (remuneracion>=25000 AND remuneracion<=250000);
+
+--Los proyectos sin fecha de finalización asignada no deben superar $100.000 de presupuesto
+
+ALTER TABLE ej_proyecto ADD CONSTRAINT ck_ej_proyecto_fecha_fin_presupuesto CHECK ((fecha_fin IS NULL AND presupuesto<=100000)
+    OR (fecha_fin IS NOT NULL));
+
+--Los tipos de especialidad DESARROLLO sólo pueden tener id_ingeniero que van del 1 al 7 para el resto no habría controles.
+ALTER TABLE ej_ingeniero ADD CONSTRAINT ck_ej_ingeniero_especialidad_id_ingeniero CHECK (id_ingeniero BETWEEN 1 AND 7);
+
+--En cada proyecto pueden trabajar 10 ingenieros como máximo.
+--chequeo que no existan mas de 10 trabajadores. Lo escribo como negacion
+ALTER TABLE ej_trabaja ADD CONSTRAINT ck_trabaja_cant_ingenieros CHECK (NOT EXISTS (SELECT id_sector, nro_proyecto FROM ej_trabaja GROUP BY id_sector, nro_proyecto HAVING count(*)>10));
+(SELECT id_sector, nro_proyecto FROM ej_trabaja GROUP BY id_sector, nro_proyecto HAVING count(*)>10);
+
+--El director asignado a un proyecto debe haber trabajado al menos en 5 proyectos ya finalizados
+CREATE ASSERTION cant_proy_fin NOT EXISTS (SELECT p.director, t.nro_proyecto FROM ej_proyecto p INNER JOIN ej_trabaja t ON (t.id_sector=p.id_sector AND t.nro_proyecto=p.nro_proyecto)
+                   WHERE fecha_fin IS NOT NULL  GROUP BY p.director, t.nro_proyecto HAVING count(*)<5);
+    (SELECT p.director, t.nro_proyecto FROM ej_proyecto p INNER JOIN ej_trabaja t ON (t.id_sector=p.id_sector AND t.nro_proyecto=p.nro_proyecto)
+                   WHERE fecha_fin IS NOT NULL  GROUP BY p.director, t.nro_proyecto HAVING count(*)<5);
